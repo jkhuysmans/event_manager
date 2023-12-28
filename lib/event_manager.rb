2a672e8b344737b45c37a990f6a08390d6348bf7
jkhuysmans <149975556+jkhuysmans@users.noreply.github.com>
2023-12-28 17:31:17 +0100

    add peak day and hour check

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -2,6 +2,7 @@
 require 'csv'
 require 'google/apis/civicinfo_v2'
 require 'erb'
+require 'time'
 
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5,"0")[0..4]
@@ -47,6 +48,20 @@ def save_thank_you_letter(id,form_letter)
   end
 end
 
+def format_time(registration_date)
+  Time.strptime(registration_date, '%m/%d/%y %k:%M')
+end
+
+def peak_hour(time)
+  hour = time.each_with_object(Hash.new(0)) { |frequency, result| result[frequency] += 1 }
+  "Peak hour is at #{hour.key(hour.values.max)} with: #{hour.values.max} people"
+end
+
+def peak_day(time)
+  day = time.each_with_object(Hash.new(0)) { |frequency, result| result[frequency] += 1 }
+  "Peak day is #{day.key(day.values.max)} with: #{day.values.max} people"
+end
+
 puts 'EventManager initialized.'
 
 contents = CSV.open(
@@ -57,7 +72,8 @@ contents = CSV.open(
 
 template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
-
+array_of_hours = []
+array_of_weekdays = [] 
 contents.each do |row|
   id = row[0]
   name = row[:first_name]
@@ -71,4 +87,11 @@ contents.each do |row|
   save_thank_you_letter(id,form_letter)
 
   puts "#{name} #{zipcode} #{numbers}"
+
+  array_of_hours << format_time(row[:regdate]).hour
+  array_of_weekdays << format_time(row[:regdate]).strftime('%A')
+
 end
+
+puts peak_hour(array_of_hours)
+puts peak_day(array_of_weekdays)
